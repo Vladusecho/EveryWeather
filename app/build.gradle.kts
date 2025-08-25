280698010a646cae4584a12d39adf76125cd490f
Vladislav Korzun <vladkorzun2016@yandex.ru>
2025-08-25 23:25:21 +0300

    Update Android Studio and project settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.vladusecho.weatherapp"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.vladusecho.weatherapp"
